e9a48ff67c26720dc295293cccfc40c9553ddd29
fabian <merkisoft@gmail.com>
2015-07-11 17:16:19 +0200

    web part started

--- web/app/scripts/app.js
+++ web/app/scripts/app.js
@@ -15,9 +15,12 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'restangular',
+    'mobile-angular-ui.core'
   ])
-  .config(function ($routeProvider) {
+
+  .config(function ($routeProvider, RestangularProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -32,4 +35,79 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  });
+
+
+    RestangularProvider.setBaseUrl('http://192.168.1.2:8080/rest');
+    // RestangularProvider.setExtraFields(['name']);
+    RestangularProvider.setResponseExtractor(function (response) {
+      /* if (response.error) {
+       alert(response.error);
+       return null;
+       }
+       return response.data;
+       */
+      return response;
+    });
+
+    RestangularProvider.setDefaultHttpFields({cache:false});
+
+    RestangularProvider.setRequestSuffix('');
+
+
+  })
+
+
+  .factory('products', ['Restangular', function (Restangular) {
+    var products = [];
+
+    function load() {
+      Restangular.all('/restaurants/2/products/groups').getList()
+        .then(function (data) {
+          angular.copy(data, products);
+        });
+
+    }
+
+    load();
+
+    return {
+      products: products
+    };
+
+  }])
+
+  .factory('order', ['Restangular', function(Restangular) {
+    var currentOrder = [];
+
+    function addItem(item) {
+      var found = false;
+      angular.forEach(currentOrder, function(val, i) {
+        if (val.id == item.id) {
+          val.size++;
+          found = true;
+        }
+      });
+
+      if (!found) {
+        item.size = 1;
+        currentOrder.push(item);
+      }
+
+    }
+
+    function submit() {
+      Restangular.all('/order').post(currentOrder)
+        .then(function(data) {
+          // $location.path("/anmeldung");
+        });
+
+    }
+
+    return {
+      addItem: addItem,
+      currentOrder: currentOrder
+    };
+
+    // return Restangular.service('order');
+  }])
+;
--- web/app/scripts/controllers/main.js
+++ web/app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the webApp
  */
 angular.module('webApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['products', '$scope', 'order', function (products, $scope, order) {
+
+    $scope.products = products.products;
+    $scope.currentOrder = order.currentOrder;
+
+    document.myscope=$scope;
+
+    $scope.addItem = function(i) {
+      order.addItem(i);
+
+    };
+
+
+  }])
+;
